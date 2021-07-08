a5debf829fdff5876ac8d1743d17683ab36010eb
Ahmad Ardiansyah <ardiansyah3ber@gmail.com>
2021-07-08 11:54:51 +0700

    Add console and set cors allow

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ require('dotenv').config();
 var server  = require('http').createServer();
 var io      = require('socket.io')(server, {
   cors: {
-    origin: "*",
+    origin: ["http://localhost:3000", "http://jual-instant.ved.carsworld.id", "https://jual-instant.ved.carsworld.id"],
     methods: ["GET", "POST", "PUT", "DELETE"]
   }
 });
 
 io.on('connection', (socket) => {
+  console.log('New user connected');
   socket.on('send', (msg) => {
     io.emit('message', msg);
   });
